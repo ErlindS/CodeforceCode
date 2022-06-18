41d0ad5ad8af2086c49dbbbf51899a2e6a2e4851
Acutus <kuriusibus@gmail.com>
2022-06-18 21:20:51 +0200

    test

---
+++ 800/1283A.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+  int x, y, z;
+  cin >> x, y, z;
+  while(x--){
+    cin >> y >> z
+    cout << 60*(23-y) + 60-z << endl;
+
+
+  }
+
+
+}
