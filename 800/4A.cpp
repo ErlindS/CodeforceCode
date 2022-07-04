ecebbafe87e58c18f052728025520fad2b0fd262
Erlind <Erlindamprogrammieren@gmail.com>
2022-07-05 00:17:52 +0200

    works.

---
+++ 800/4A.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+  int x;
+  cin >> x;
+  if(x == 2){cout << "NO"; return 0;}
+  if(x % 2 == 0){cout << "YES"; return 0; }
+  cout << "NO";
+
+
+}
